2a2bd9ea385f8014b6e966e9bc76cc9bfa109ec3
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-03 16:34:36 -0300

    feat(i18n): add intl to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
+import { setIntlConfig, withIntl } from 'storybook-addon-intl'
+// Preview layout
 import Layout from './Layout';
+// i18n
+import translations from '../src/i18n'
 
+const getMessages = locale => translations[locale]
+
+setIntlConfig({
+  locales: ['en-US'],
+  defaultLocale: 'en-US',
+  getMessages
+})
+
+addDecorator(withIntl)
 addDecorator(storyFn => <Layout>{storyFn()}</Layout>);
+
+
+
+
+
+
