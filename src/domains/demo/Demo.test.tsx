d681b62ee8247f8eb05bff30e506a130db70b39c
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-03 07:57:04 -0300

    feat: init boilerplate with routing

---
+++ src/domains/demo/Demo.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Demo from './';
+
+test('renders hello world', () => {
+  const { getByText } = render(<Demo />);
+  const greeting = getByText(/Hello World!/i);
+  expect(greeting).toBeInTheDocument();
+});
---
+++ src/router/index.tsx
@@ -0,0 +1,9 @@
+import { Demo } from '../domains';
+
+export const routes: Array<Object> = [
+    {
+        path: "/",
+        exact: true,
+        component: Demo
+    },
+];
