448ed834142bd42ca0c65ee6648692f8c53a97cd
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-05 06:39:30 -0300

    feat: improve input

--- src/app/components/Input/Input.tsx
+++ src/app/components/Input/Input.tsx
@@ -1,24 +1,28 @@
 import React from "react";
+import { FormError } from "../Form/FormError";
 
 type Props = {
 	type: string;
 	label: string;
 	name: string;
+	error: string;
+	innerSlot: React.ReactNode;
 };
 
-const Input = ({ type, label, name }: Props) => (
+const Input = ({ type, label, name, error, innerSlot }: Props) => (
 	<div className="flex flex-col">
-		<label className="mb-2 text-theme-medium">
+		<label className="text-theme-medium">
 			{label}
-			<div className="flex relative items-center">
-				<input className="flex-1 border border-theme-neutral-light rounded-md h-12 w-full" type={type} name={name} />
-				{/* {innerSlot && 
-                <button v-if="icon" className="absolute right-0 mr-4 h-12 text-blue-600">
-                    <SvgIcon :name="icon" view-box="0 0 20 20" />
-                </button>
-            } */}
+			<div className="mt-2 pb-2 flex relative items-center">
+				<input
+					className="flex-1 border border-theme-neutral-light rounded-md h-12 w-full"
+					type={type}
+					name={name}
+				/>
+				{innerSlot && <div className="absolute right-0 mr-4 mt-1">{innerSlot}</div>}
 			</div>
 		</label>
+		{error && <FormError error={error} />}
 	</div>
 );
 
@@ -28,4 +32,4 @@ Input.defaultProps = {
 	name: "input",
 };
 
-export default Input;
+export { Input };
