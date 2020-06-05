2dc5d2ac5631799981a5fde2b922dbec7133c6ab
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-05 07:37:35 -0300

    feat: wip add import wallet page

---
+++ src/domains/wallets/pages/ImportWallet/ImportWallet.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ImportWallet } from "./ImportWallet";
+
+export default {
+	title: "Pages / Wallets / Import Wallet",
+};
+
+export const Default = () => (
+	<div className="w-full h-full">
+		<ImportWallet />
+	</div>
+);
