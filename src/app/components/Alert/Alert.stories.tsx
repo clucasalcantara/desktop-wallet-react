ddc18688164028c52c97a4b0105cf7e50b6d4230
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-06-05 05:33:29 -0300

    fix: knobs with local variables

--- src/app/components/Alert/Alert.stories.tsx
+++ src/app/components/Alert/Alert.stories.tsx
@@ -1,36 +1,21 @@
 import React from "react";
 import Alert from "./Alert";
-import { withKnobs, text, select } from "@storybook/addon-knobs";
+import { withKnobs, select, text } from "@storybook/addon-knobs";
 
 export default {
 	title: "Components / Alert",
-	decorators: [withKnobs],
+	decorators: [withKnobs]
 };
 
-const editableProps = {
-	title: text("Title", "Title"),
-	children: text("Content", "Content"),
-	size: select(
-		"Size",
-		{
-			Small: "small",
-			Default: "default",
-			Large: "large",
-		},
-		"default",
-	),
-	status: select(
-		"Variant",
-		{
-			Primary: "primary",
-			Warning: "warning",
-			Success: "success",
-			Danger: "danger",
-		},
-		"warning",
-	),
-};
+export const Default = () => {
+	const variant = select("Variant", ["primary","success","warning","danger","neutral"], "warning");
+	const size = select("Size", ["small", "default", "large"], "default");
+	const title = text("Title", "Titlle");
 
-export const Default = () => (
-	<Alert {...editableProps} />
-);
+	return (
+		<Alert variant={variant} size={size} title={title}>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda molestias voluptatibus minus eveniet
+			cumque?
+		</Alert>
+	);
+};
--- src/domains/profile/components/ProfileCard/ProfileCard.stories.tsx
+++ src/domains/profile/components/ProfileCard/ProfileCard.stories.tsx
@@ -4,10 +4,10 @@ import { withKnobs, text } from "@storybook/addon-knobs";
 
 export default { title: "Components / Profile Card", decorators: [withKnobs] };
 
-const editableProps = {
-	name: text("Profile name", "Oleg Gelo"),
-	balance: text("Total Balance", "234,500.46 USD"),
-	avatar: text("User Avatar", "https://www.w3schools.com/howto/img_avatar.png"),
-};
+export const Default = () => {
+	const name = text("Profile name", "Oleg Gelo");
+	const balance = text("Total Balance", "234,500.46 USD");
+	const avatar = text("User Avatar", "https://www.w3schools.com/howto/img_avatar.png");
 
-export const Default = () => <ProfileCard {...editableProps} />;
+	return <ProfileCard name={name} balance={balance} avatar={avatar} />;
+};
